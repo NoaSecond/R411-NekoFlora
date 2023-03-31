16e75022e9b044b44b19d01451f6f41db8a01575
sn103354 <noa.second@etu.unice.fr>
2023-03-31 12:05:25 +0200

    Update Main, Cart and Product activities

---
+++ app/src/main/java/com/noasecond/nekoflora/ProductActivity.java
@@ -0,0 +1,14 @@
+package com.noasecond.nekoflora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+    }
+}
